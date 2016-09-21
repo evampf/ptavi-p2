2295ad7b051eb3daa8c8bdf16bfbe081c46bfdef
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-09-21 13:51:23 +0200

    Entrega calcoo

--- calcoo.py
+++ calcoo.py
@@ -5,11 +5,11 @@ import sys
 
 class calculadora():
 
-	def suma(num1,num2):
+	def suma(self,num1,num2):
 		"""Funcion que suma números"""
 		return num1 + num2
 
-	def resta(num1,num2):
+	def resta(self,num1,num2):
 		"""Función que resta números"""
 		return num1 - num2
 
@@ -21,9 +21,9 @@ if __name__ == "__main__":
         sys.exit("Error: Parámetros incorrectos")
 
     if sys.argv[2] == "suma":
-        result = calculadora.suma(numero1, numero2)
+        result = calculadora().suma(numero1, numero2)
     elif sys.argv[2] == "resta":
-        result = calculadora.resta(numero1, numero2)
+        result = calculadora().resta(numero1, numero2)
 
     else:
         sys.exit('Operación sólo puede ser sumar o restar.')
