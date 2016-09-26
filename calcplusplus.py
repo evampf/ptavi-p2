23c3c404d60517b1e1dd5dc65416ba704d3fd15f
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-Eva.local>
2016-09-26 17:29:32 +0200

    entrega calcplusplus.py

---
+++ calcplusplus.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import csv
+import calcoohija
+
+if __name__ == "__main__":
+
+    datos = sys.argv[1]
+    with open(datos) as operaciones:
+
+        datos = csv.reader(operaciones)
+        calculadora2 = calcoohija.CalculadoraHija()
+    
+        for linea in datos:
+            operacion = linea[0]
+            numeros = linea[1:]
+            resultado = int(numeros[0])
+
+            if operacion == "suma":
+                for num in numeros[1:]:
+                    resultado = calculadora2.suma(resultado, int(num))
+            elif operacion == "resta":
+                for num in numeros[1:]:
+                    resultado = calculadora2.resta(resultado, int(num))
+            elif operacion == "multiplica":
+                for num in numeros[1:]:
+                    resultado = calculadora2.multiplica(resultado, int(num))
+            elif operacion == "divide":
+                for num in numeros[1:]:
+                    resultado = calculadora2.divide(resultado, int(num))
+
+            else:
+                sys.exit('Operación sólo puede ser sumar, restar, multiplicar o dividir.')
+
+            print(resultado)
+
+
+	
